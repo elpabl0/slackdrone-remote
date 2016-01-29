4715f17009464d4db2325c4c392351e3ad32170b
elpabl0 <pauladdicottevans@Pauls-MacBook-Pro.local>
2016-01-29 15:25:32 +0000

    Heroku / sockets

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 var port = process.env.PORT || 8080;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,6 +13,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(port, function(){
+server.listen(port, function(){
   console.log('listening on '+ port);
 });
