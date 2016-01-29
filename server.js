02e60adfee4cb29961e0e6c1011887319980193d
elpabl0 <pauladdicottevans@Pauls-MacBook-Pro.local>
2016-01-29 13:55:04 +0000

    dynamic port

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(port, function(){
+  console.log('listening on '+ port);
 });
