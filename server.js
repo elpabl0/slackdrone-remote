926da92c92b9b52aa85c436a47aa21f298e2a8f6
elpabl0 <pauladdicottevans@Pauls-MacBook-Pro.local>
2016-01-29 15:37:24 +0000

    herokuy sockets?

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
   socket.on('chat message', function(msg)
   {
     io.emit('chat message', msg);
